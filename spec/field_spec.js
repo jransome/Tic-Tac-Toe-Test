fa363e1c05b6c0256150f4ccfbabde05968f9e7e
James Ransome <jamesransome@msn.com>
2017-09-07 15:23:25 +0100

    passing test for field

---
+++ spec/field_spec.js
@@ -0,0 +1,22 @@
+describe("Field", function(){
+  var field;
+
+  beforeEach(function(){
+    field = new Field();
+  });
+
+  it("is empty by default", function(){
+    expect(field.claimedBy()).toEqual(null);
+  });
+
+  it("can be claimed by a player", function(){
+    field.claim('X');
+    expect(field.claimedBy()).toEqual('X');
+  });
+
+  it("throws an error a player attempts to claim an already claimed field", function(){
+    field.claim('X');
+    expect(function(){ field.claim('O'); }).toThrow("Field is already claimed!");
+  });
+
+});
---
+++ src/field.js
@@ -0,0 +1,22 @@
+(function(exports){
+
+  function Field(){
+    this._claimedBy = null;
+  }
+
+  Field.prototype = {
+    claimedBy: function(){
+      return this._claimedBy;
+    },
+
+    claim: function(marker){
+      if(this._claimedBy === null) {
+        this._claimedBy = marker;
+      } else {
+        throw "Field is already claimed!";
+      }
+    }
+  };
+
+  exports.Field = Field;
+})(this);
