723b2d877a725c01bdc1c4cdd71c63e5a93891c3
James Ransome <jamesransome@msn.com>
2017-09-10 11:20:29 +0100

    add board debugger to helpers

--- spec/helpers.js
+++ spec/helpers.js
@@ -9,3 +9,26 @@ function constructEmptyBoardDouble(fieldClassDouble){
   }
   return board;
 }
+
+function boardDebugger(board){
+  var output = [];
+  for (var i = 0; i < BOARD_HEIGHT_AND_WIDTH; i++) {
+    var newRow = [];
+    for (var j = 0; j < BOARD_HEIGHT_AND_WIDTH; j++) {
+      newRow.push(markerPrinter(board.checkMarker(i, j)));
+    }
+    output.push(newRow);
+  }
+  return output;
+}
+
+function markerPrinter(isCross){
+  var markerString = " ";
+  if (isCross) {
+    markerString = "X";
+  }
+  else if (isCross === false){ // as opposed to null
+    markerString = "O";
+  }
+  return markerString;
+}
